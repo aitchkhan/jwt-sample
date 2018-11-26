d4e98292501c0f8634a087cc0df1b4a833d21a21
Haroon Khan <haroon.khan@tenpearls.com>
2018-11-26 15:45:59 +0500

    get all user api

--- controllers/user.js
+++ controllers/user.js
@@ -34,9 +34,117 @@ module.exports = function(router) {
 
     router.get('/scim/v2/Users', require('../middlewares/auth.js'), function(req, res) {
         console.log('I AM BAT MAN and a GET CALL USER');
-        res.status(404).json({
-            message: 'this is message'
-        });
+        const users = [{
+            "schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
+            "id":"2819c223-7f76-453a-919d-413861904646",
+            "externalId":"bjensen",
+            "meta":{
+              "resourceType": "User",
+              "created":"2011-08-01T18:29:49.793Z",
+              "lastModified":"2011-08-01T18:29:49.793Z",
+              "location":"https://example.com/v2/Users/2819c223...",
+              "version":"W\/\"f250dd84f0671c3\""
+            },
+            "name":{
+              "formatted": "Ms. Barbara J Jensen, III",
+              "familyName": "Jensen",
+              "givenName": "Barbara",
+              "middleName": "Jane",
+              "honorificPrefix": "Ms.",
+              "honorificSuffix": "III"
+            },
+            "userName":"bjensen",
+            "phoneNumbers":[
+              {
+                "value":"555-555-8377",
+                "type":"work"
+              }
+            ],
+            "emails":[
+              {
+                "value":"bjensen@example.com",
+                "type":"work",
+                "primary": true
+              }
+            ]
+          },
+          {
+            "schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
+            "id":"2819c223-7f76-453a-919d-413861904646",
+            "externalId":"bjensen",
+            "meta":{
+              "resourceType": "User",
+              "created":"2011-08-01T18:29:49.793Z",
+              "lastModified":"2011-08-01T18:29:49.793Z",
+              "location":"https://example.com/v2/Users/2819c223...",
+              "version":"W\/\"f250dd84f0671c3\""
+            },
+            "name":{
+              "formatted": "Ms. Barbara J Jensen, III",
+              "familyName": "Jensen",
+              "givenName": "Barbara",
+              "middleName": "Jane",
+              "honorificPrefix": "Ms.",
+              "honorificSuffix": "III"
+            },
+            "userName":"bjensen",
+            "phoneNumbers":[
+              {
+                "value":"555-555-8377",
+                "type":"work"
+              }
+            ],
+            "emails":[
+              {
+                "value":"bjensen@example.com",
+                "type":"work",
+                "primary": true
+              }
+            ]
+          }];
+
+        res.status(200).json(users);
+    });
+
+    router.get('/scim/v2/Users/:id', require('../middlewares/auth.js'), function(req, res) {
+        console.log(req.params.id);
+        console.log('I AM BAT MAN and a GET CALL USER');
+        const user = {
+            "schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
+            "id":"2819c223-7f76-453a-919d-413861904646",
+            "externalId":"bjensen",
+            "meta":{
+              "resourceType": "User",
+              "created":"2011-08-01T18:29:49.793Z",
+              "lastModified":"2011-08-01T18:29:49.793Z",
+              "location":"https://example.com/v2/Users/2819c223...",
+              "version":"W\/\"f250dd84f0671c3\""
+            },
+            "name":{
+              "formatted": "Ms. Barbara J Jensen, III",
+              "familyName": "Jensen",
+              "givenName": "Barbara",
+              "middleName": "Jane",
+              "honorificPrefix": "Ms.",
+              "honorificSuffix": "III"
+            },
+            "userName":"bjensen",
+            "phoneNumbers":[
+              {
+                "value":"555-555-8377",
+                "type":"work"
+              }
+            ],
+            "emails":[
+              {
+                "value":"bjensen@example.com",
+                "type":"work",
+                "primary": true
+              }
+            ]
+          };
+
+        res.status(200).json(user);
     });
 
     return router;
